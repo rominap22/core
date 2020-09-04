065c9d4629a8fea1fd1d9b798243fb9b54c53c38
Christian Sparks <csparks1919@hotmail.com>
2020-09-04 13:19:37 -0500

    Add the ability to support multiple meeting times for SIGs (#28) (#69)

--- model/group.go
+++ model/group.go
@@ -1,14 +1,19 @@
 package model
 
 type Group struct {
-	Name            string        `yaml:"name"`
-	Description     string        `yaml:"description"`
-	Chairs          string        `yaml:"chairs"`
-	Members         []GroupMember `yaml:"members"`
-	MeetingTime     string        `yaml:"meetingTime"`
-	MeetingLocation string        `yaml:"meetingLocation"`
-	Website         string        `yaml:"website"`
-	Email           string        `yaml:"email"`
+	Name        string        `yaml:"name"`
+	Description string        `yaml:"description"`
+	Chairs      string        `yaml:"chairs"`
+	Meetings    []Meeting     `yaml:"meetings"`
+	Members     []GroupMember `yaml:"members"`
+	Website     string        `yaml:"website"`
+	Email       string        `yaml:"email"`
+}
+
+type Meeting struct {
+	Topic           string `yaml:"topic"`
+	MeetingTime     string `yaml:"meetingTime"`
+	MeetingLocation string `yaml:"meetingLocation"`
 }
 
 type GroupMember struct {
